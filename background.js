12a1741971876179089a278ea78cd3ef5e2221f2
Sep Keuchenius <sep@y.digital>
2021-10-26 14:19:59 +0200

    functions relating to the commands

--- background.js
+++ background.js
@@ -5,4 +5,21 @@ let color = '#3aa757';
 chrome.runtime.onInstalled.addListener(() => {
   chrome.storage.sync.set({ color });
   console.log('Default background color set to %cgreen', `color: ${color}`);
+    // chrome.storage.sync.set({"checkpoints": []})
+});
+
+chrome.commands.onCommand.addListener((command) => {
+    console.log(`Command: ${command}`);
+    if(command == 'save_checkpoint'){
+        chrome.storage.sync.get({"checkpoints": []}, function(result){
+            current_checkpoints = result.checkpoints;
+            console.log(current_checkpoints);
+            current_checkpoints.push(new Date().getTime());
+            chrome.storage.sync.set({"checkpoints": current_checkpoints})
+        })
+    }
+    else if(command == "clear_checkpoints"){
+        chrome.storage.sync.remove("checkpoints")
+    }
+    
 });
