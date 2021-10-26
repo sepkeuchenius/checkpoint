61fc0139fe4fc71d37112176448ea7c4e821a709
Sep Keuchenius <sep@y.digital>
2021-10-26 15:11:30 +0200

    checkpoints being loaded in popup

--- popup.js
+++ popup.js
@@ -4,3 +4,14 @@ let changeColor = document.getElementById("changeColor");
 chrome.storage.sync.get("color", ({ color }) => {
   changeColor.style.backgroundColor = color;
 });
+
+
+chrome.storage.sync.get("checkpoints", function(result){
+    checkpoints = result.checkpoints;
+    for(point of checkpoints){
+        console.log(point)
+        var el = document.createElement('div');
+        el.innerText = point.selection;
+        document.getElementById("checkpoints").appendChild(el);
+    }
+})
