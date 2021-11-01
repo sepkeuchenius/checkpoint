dd07686a88df9b358777a492121928cd9a2699ad
Sep Keuchenius <sep@y.digital>
2021-11-01 20:08:22 +0100

    scrolling down on checkpoint open

--- background.js
+++ background.js
@@ -11,16 +11,20 @@ chrome.runtime.onInstalled.addListener(() => {
 chrome.commands.onCommand.addListener((command) => {
     console.log(`Command: ${command}`);
        if(command == 'save_checkpoint'){
-       saveSelection()
+       saveTab()
     }
-    else if(command == "clear_checkpoints"){
-        chrome.storage.sync.remove("checkpoints")
-    }
+    // else if(command == "clear_checkpoints"){
+    //     chrome.storage.sync.remove("checkpoints")
+    // }
     
 });
 
+
 function getSelection(){
     return window.getSelection().toString();
+}
+function getScroll(){
+    return window.pageYOffset;
 }
 function saveSelection(){
    return chrome.tabs.query({ currentWindow: true, active: true }, function (tabs) {
@@ -61,3 +65,115 @@ function saveSelection(){
         });
       });
 }
+
+async function getCurrentTab() {
+    let queryOptions = { active: true, currentWindow: true };
+    let [tab] = await chrome.tabs.query(queryOptions);
+    return tab;
+  }
+
+
+async function getTabSelection(tab) {
+    let [result] = await chrome.scripting.executeScript(
+        {
+          target: {tabId: tab.id, allFrames: true},
+          func: getSelection,
+        });
+    return result.result;
+}
+
+async function getTabScroll(tab) {
+    let [result] = await chrome.scripting.executeScript(
+        {
+          target: {tabId: tab.id, allFrames: true},
+          func: getScroll,
+        });
+    return result.result;
+}
+
+async function saveTab(){
+    var current =  await getCurrentTab();
+    var selection = await getTabSelection(current);
+    var url = current.url;
+    var scroll = await getTabScroll(current);
+    //save the selection in chrome sync storage
+    chrome.storage.sync.get({"checkpoints": []}, function(result){
+        current_checkpoints = result.checkpoints;
+        console.log(current_checkpoints);
+
+        var checkpoint = {
+            'created': new Date().getTime(),
+            'selection': selection,
+            'url': url,
+            'scroll':  scroll,
+            "id": (Math.random() * 10000).toFixed(0)
+        }
+
+        current_checkpoints.push(checkpoint);
+        chrome.storage.sync.set({"checkpoints": current_checkpoints}, function(res){
+            chrome.notifications.create({
+                "title" : "Checkpoint saved",
+                "message" : "Your checkpoint was saved succesfully",
+                "iconUrl": "icon_128_mc_2.png",
+                "type": "basic",
+            })
+        })
+    });
+}
+var functionMapping = 
+{
+    'scroll': scrollDown
+};
+
+
+chrome.runtime.onMessage.addListener(
+    function(request, sender, sendResponse) {
+      console.log('called')
+      if (request.type === "function"){
+        if(request.function == 'openCheckpoint'){
+            //open checkpoint request from extension script
+            openCheckpoint(request.checkpoint);
+        }
+    }
+        sendResponse({success: true});
+    }
+  );
+
+async function openCheckpoint(checkpoint){
+    //open window and scroll down
+    await openWindow(checkpoint.url, checkpoint.scroll);
+    await scrollDown(checkpoint.scroll);
+}
+
+async function openWindow(url,to){
+    await chrome.tabs.create({
+        url: url
+    })
+}
+
+async function scrollDown(to){
+    //listen for the tab to be created
+    chrome.tabs.onUpdated.addListener(function(tabId){
+        chrome.scripting.executeScript({
+            target :{tabId : tabId, allFrames : true,
+            },
+            args:[to],
+            func: function(to){
+                //scroll down
+                window.scrollTo(0, to)
+            }
+        });
+        chrome.tabs.onUpdated.removeListener(arguments.callee)
+
+    })
+    
+}
+
+
+async function getCurrentTab() {
+    let queryOptions = { active: true, currentWindow: true };
+    let [tab] = await chrome.tabs.query(queryOptions);
+    return tab;
+}
+  
+  
--- popup.js
+++ popup.js
@@ -5,69 +5,77 @@ chrome.storage.sync.get("color", ({ color }) => {
   changeColor.style.backgroundColor = color;
 });
 
+checkpoints = []
+function loadCheckpoints(){
+  chrome.storage.sync.get("checkpoints", function(result){
+      checkpoints = result.checkpoints;
+      var now = new Date().getTime();
+      checkpoints.reverse();
+      console.log(checkpoints)
+      for(point of checkpoints){
+          console.log(point)
+          //create checkpoint element
+          var el = document.createElement('div');
+          el.className = "checkpoint";
+          if(point["id"]){
+            el.id = point.id;
+          }
 
-chrome.storage.sync.get("checkpoints", function(result){
-    checkpoints = result.checkpoints;
-    var now = new Date().getTime();
-    for(point of checkpoints){
-        console.log(point)
-        //create checkpoint element
-        var el = document.createElement('div');
-        el.className = "checkpoint";
-        el.id = point.url;
-
-        var textEl = document.createElement('p');
-        textEl.innerText = point.selection;
-        textEl.className = 'selection';
-        el.appendChild(textEl)
-        //create sub elements
-        var time = document.createElement('p');
-        
-        var ago = ""
-        var seconds_ago = (now - point.created) / 1000
-        var minutes_ago = seconds_ago / 60;
-        var hours_ago = minutes_ago / 60;
-        var days_ago = hours_ago / 24;
-        ago = Math.round(days_ago) + ' days ago';
-        if(hours_ago < 24){
-          ago = Math.round(hours_ago) + ' hours ago';
-        }
-        if(minutes_ago < 60){
-          ago = Math.round(minutes_ago) + ' minutes ago';
-        }
-        if(seconds_ago < 60){
-          ago = Math.round(seconds_ago) + ' seconds ago';
-        }
-        
+          var textEl = document.createElement('p');
+          textEl.innerText = point.selection;
+          textEl.className = 'selection';
+          el.appendChild(textEl)
+          //create sub elements
+          var time = document.createElement('p');
           
-        time.className = "time"
-        time.innerText = ago;
+          var ago = ""
+          var seconds_ago = (now - point.created) / 1000
+          var minutes_ago = seconds_ago / 60;
+          var hours_ago = minutes_ago / 60;
+          var days_ago = hours_ago / 24;
+          ago = Math.round(days_ago) + ' days ago';
+          if(hours_ago < 24){
+            ago = Math.round(hours_ago) + ' hours ago';
+          }
+          if(minutes_ago < 60){
+            ago = Math.round(minutes_ago) + ' minutes ago';
+          }
+          if(seconds_ago < 60){
+            ago = Math.round(seconds_ago) + ' seconds ago';
+          }
+          
+            
+          time.className = "time"
+          time.innerText = ago;
 
-        el.appendChild(time)
+          el.appendChild(time)
 
 
-        //create buttons 
-        var del = document.createElement('button');
-        var copy = document.createElement('button');
+          //create buttons 
+          var del = document.createElement('button');
+          var copy = document.createElement('button');
 
-        del.innerText = "Delete";
-        copy.innerText = "Copy";
+          del.innerText = "Delete";
+          copy.innerText = "Copy";
 
-        del.className = "small-button"
-        copy.className = "small-button"
+          del.className = "small-button"
+          copy.className = "small-button"
 
-        del.style.right = "4px";
-        copy.style.right = "49px";
+          del.style.right = "4px";
+          copy.style.right = "49px";
 
 
-        el.appendChild(del)
-        el.appendChild(copy)
+          el.appendChild(del)
+          el.appendChild(copy)
 
-        document.getElementById("checkpoints").appendChild(el);
-    }
-})
+          document.getElementById("checkpoints").appendChild(el);
+          addListeners()
+      }
+  })
+}
 
 window.onload = function(){
+  loadCheckpoints();
   addListeners();
 }
 
@@ -93,9 +101,53 @@ function checkpointHoverOut(e){
     element.style.display = 'none';
   }); 
 }
-function checkpointClick(e){
+async function checkpointClick(e){
+  //get the clicked checkpoint id
+  var id = null
   if(e.target.className == 'selection' || e.target.className == 'small-button' || e.target.className == 'time'){
-    window.open(e.target.parentElement.id)
+    id = e.target.parentElement.id;
   }
-  // window.open(e.target.id)
+  else{
+    id = e.target.id
+  }
+  //get the checkpoint
+  var checkpoint = checkpoints.filter(function(c){return c.id == id});
+  
+  if(checkpoint.length > 0){
+    checkpoint = checkpoint[0]
+    //call background to run the open workflow
+    chrome.runtime.sendMessage({
+      type:"function",
+      function:'openCheckpoint',
+      checkpoint: checkpoint
+    }, function(response) {
+      console.log(response.success);
+    });
+    
+  }
+  
+  
 }
+
+
+async function getCurrentTab() {
+  let queryOptions = { active: true, currentWindow: true };
+  let [tab] = await chrome.tabs.query(queryOptions);
+  return tab;
+}
+
+
+chrome.runtime.onMessage.addListener(
+  function(request, sender, sendResponse) {
+    if(request.type == 'function'){
+      if(request.function == 'openWindow'){
+        window.open(request.url);
+        window.setTimeout(function(){
+          sendResponse({success:true})
+        },500)
+      }
+    }
+  });
+
+
+
